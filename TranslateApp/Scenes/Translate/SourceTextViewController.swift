c5919fd56e17af2446ae817e76558452293f4dd0
jmkim <wjdals0304@gmail.com>
2022-03-17 23:17:03 +0900

    add: 텍스트뷰 클릭 뷰 컨트롤러 추가

---
+++ TranslateApp/Scenes/Translate/SourceTextViewController.swift
@@ -0,0 +1,77 @@
+//
+//  SourceTextViewController.swift
+//  TranslateApp
+//
+//  Created by 김정민 on 2022/03/17.
+//
+
+import UIKit
+import SnapKit
+
+protocol SourceTextViewControllerDelegate : AnyObject {
+    func didEnterText(_ sourceText: String)
+}
+
+
+final class SourceTextViewController: UIViewController {
+    
+    private let placeholderText = "텍스트 입력"
+    
+    private weak var delegate: SourceTextViewControllerDelegate?
+
+    private lazy var textView: UITextView = {
+        let textView = UITextView()
+        textView.text = placeholderText
+        textView.textColor = .secondaryLabel
+        textView.font = .systemFont(ofSize: 18.0, weight: .semibold)
+        textView.returnKeyType = .done
+        textView.delegate = self
+        return textView
+    }()
+    
+    init(delegate: SourceTextViewControllerDelegate?) {
+        self.delegate = delegate
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .systemBackground
+        
+        view.addSubview(textView)
+        textView.snp.makeConstraints { make in
+            make.edges.equalToSuperview().inset(16.0)
+        }
+    }
+    
+    
+}
+
+
+extension SourceTextViewController: UITextViewDelegate {
+    
+    func textViewDidBeginEditing(_ textView: UITextView) {
+        guard  textView.textColor == .secondaryLabel else { return }
+        
+        textView.text = nil
+        textView.textColor = .label
+        
+        
+    }
+    
+    func textView(_ textView: UITextView, shouldChangeTextIn range: NSRange, replacementText text: String) -> Bool {
+        guard text == "\n" else { return true }
+        
+        delegate?.didEnterText(textView.text)
+        dismiss(animated: true, completion: nil)
+        
+        return true
+    }
+    
+    
+}
