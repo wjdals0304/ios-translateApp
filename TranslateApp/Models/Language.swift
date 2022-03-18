5ce318db15efe964483eb9154612db61d33b5045
jmkim <wjdals0304@gmail.com>
2022-03-18 15:51:33 +0900

    add: 국제화 적용

--- TranslateApp/Models/Language.swift
+++ TranslateApp/Models/Language.swift
@@ -13,14 +13,19 @@ enum Language: String, CaseIterable, Codable {
     case ko
     case en
     case ja
-    case ch
+    case ch = "zn-CN"
     
     var title: String {
         switch self {
-        case .ko: return "한국어"
-        case .en: return "영어"
-        case .ja: return "일본어"
-        case .ch: return "중국어"
+        case .ko: return NSLocalizedString("Korean", comment: "한국어")
+        case .en: return NSLocalizedString("English", comment: "영어")
+        case .ja: return NSLocalizedString("Japanese", comment: "일본어")
+        case .ch: return NSLocalizedString("Chinese", comment: "중국")
         }
     }
+    
+    var languageCode: String {
+        self.rawValue
+    }
+    
 }
